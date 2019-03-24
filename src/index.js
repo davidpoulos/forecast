43a2645bbf552a1ad202780b27a43f63465bc204
David Stephen Poulos <davidstephenpoulos@gmail.com>
2019-03-24 11:23:08 -0500

    Adding a logger and basic User Model

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ const cluster = require('cluster');
 
 const config = require('./config/core');
 
+const { connectToMongo } = require('./client/mongo');
 const { app } = require('./server');
 const numCPUs = os.cpus().length;
 
 
-(() => {
+(async () => {
 	if(cluster.isMaster) {
+
+		await connectToMongo();
+
 		for(let i = 0; i < numCPUs; i++) {
 			cluster.fork();
 		}
