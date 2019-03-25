878d8c7964e6e63b0146b601d45c4ac23d2a32a2
David Stephen Poulos <davidstephenpoulos@gmail.com>
2019-03-24 23:28:37 -0500

    (mongodb) establish connection to docker-compose mongo

---
+++ src/client/mongo.js
@@ -0,0 +1,20 @@
+const logger = require('../logger');
+const mongoose = require('mongoose');
+
+/**
+ * Connects Mongoose to MongoDB. Once done, all instances of Mongoose automically use this connection
+ */
+async function connectToMongo() {
+	logger.info('=> Now connecting to MongoDB');
+	try {
+		await mongoose.connect('mongodb://mongodb:27017/forecast', { useNewUrlParser: true });
+	} catch(error) {
+		logger.error(`Un-able to establish MongoDB connection. ${error.message}`);
+	}
+	
+	logger.info('=> MongoDB connection established.');
+}
+
+module.exports = {
+	connectToMongo
+};
